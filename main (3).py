5cb1116661194bc0f377b5b563474b5df52a7373
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-01-20 16:41:25 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,20 @@
+import threading
+
+n = int(input("enter number :"))
+lock = threading.Lock()
+def print_1_n(n):  
+  for i in range(1,n+1):
+    lock.acquire()
+    print(threading.current_thread().getName() +" "+ str(i))
+    lock.release()
+
+  
+
+th1 = threading.Thread(target= print_1_n,args=(n,))
+
+th2 = threading.Thread(target=print_1_n,args=(n,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
